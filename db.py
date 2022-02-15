d4cc9594765ae34ae0d1afd018184d6be278e31d
minpeter <kali2005611@gmail.com>
2022-02-15 23:45:29 +0900

    remove api.py(not use)

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
 import pymysql
 
+
 def db():
     return pymysql.connect(
-        host='127.0.0.1', 
-        user='root', 
-        password='abcdefg',
-        db='war-game', 
-        charset='utf8')
+        host='127.0.0.1',
+        user='root',
+        password='abcdefu',
+        db='war-game',
+        charset='utf8'
+    )
