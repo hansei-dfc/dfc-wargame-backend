874a24087bda9372dc3fe6638ef8bb754abcbc83
minpeter <kali2005611@gmail.com>
2022-02-15 22:46:20 +0900

    /app.py update

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
+from flask import Flask, jsonify
 from dotenv import load_dotenv
+from flask_restx import Resource, Api
+from auth import Auth
 
-from auth.db.email_verify import check_verify_code, db_create_verify, is_verified
-from auth.db.users import create_user
+
 load_dotenv()
 
-from flask import Flask
-from auth.auth import auth
-
 app = Flask(__name__)
+api = Api(
+    app,
+    version='0.1',
+    title="Minpeter's API Server",
+    description="Minpeter's sandbox API Server!",
+    terms_url="/",
+    contact="kali2005611@gmail.com",
+    license="MIT"
+)
 
-app.register_blueprint(auth)
+api.add_namespace(Auth, '/auth')
+
 
 @app.route('/')
-def board():
-    return "index"
+def home():
+    return jsonify({"massage": 'This api is the api that creates the sandbox of hansei wargame.'})
 
-app.run(host="0.0.0.0", port=5001, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=False)
---
+++ readme.md
@@ -0,0 +1,12 @@
+## .env **needs** to be created
+
+```
+smtp_user=
+smtp_sender=
+smtp_pass=
+smtp_server=[ex) smtp.gmail.com]
+smtp_port=[ex) 587]
+pass_salt=[string]
+server_url=[backend server_ip or domain]
+email_vefify_title=hansei WarGame Email Verification
+```
