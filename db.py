af3e04ce42887fe7d6741014e036ce583072e6d9
minpeter <kali2005611@gmail.com>
2022-02-15 23:59:04 +0900

    users.py +> db.py

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 import pymysql
+import hashlib
+import os
 
 
 def db():
@@ -9,3 +11,36 @@ def db():
         db='war-game',
         charset='utf8'
     )
+
+
+def is_user_exists(userId):
+    conn = db()
+    cus = conn.cursor()
+    cus.execute(f"select exists(select 1 from `users` where `id`='{userId}')")
+    conn.close()
+    return cus.fetchone()[0] == 1
+
+
+def is_user_exists_email(email):
+    conn = db()
+    cus = conn.cursor()
+    cus.execute(f"select exists(select 1 from `users` where `email`=%s)", email)
+    conn.close()
+    return cus.fetchone()[0] == 1
+
+
+def create_user(name, email, password):
+    password = hashlib.sha256(
+        (password + os.environ.get('pass_salt')).encode()).hexdigest()
+    conn = db()
+    cus = conn.cursor()
+    if (cus.execute(f"insert into `users` (`name`, `email`, `password`) values (%s, %s, %s)",
+                    (name, email, password)) != 1):
+        return None
+    conn.commit()
+
+    cus = conn.cursor()
+    cus.execute(f"select `id` from `users` where email=%s and password=%s",
+                (email, password))
+    conn.close()
+    return cus.fetchone()[0]
