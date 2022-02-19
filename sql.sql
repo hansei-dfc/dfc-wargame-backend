51b1b324fae34e55b7a93d591aa98865ce96dd07
Soju06 <34199905+Soju06@users.noreply.github.com>
2022-02-19 18:59:42 +0900

    notices, notice_views 추가

--- sql.sql
+++ sql.sql
@@ -8,6 +8,23 @@
 CREATE DATABASE IF NOT EXISTS `war-game` /*!40100 DEFAULT CHARACTER SET armscii8 COLLATE armscii8_bin */;
 USE `war-game`;
 
+CREATE TABLE IF NOT EXISTS `notices` (
+  `notice_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'notice id',
+  `author_id` bigint(20) unsigned NOT NULL COMMENT '작성자',
+  `notice_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '타이틀',
+  `notice_contents` text CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL COMMENT '콘텐츠',
+  `notice_type` enum('title','topbar','message','none') COLLATE armscii8_bin NOT NULL COMMENT '타입',
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '작성 날짜, 수정 날짜',
+  PRIMARY KEY (`notice_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin COMMENT='notices';
+
+CREATE TABLE IF NOT EXISTS `notice_views` (
+  `notice_id` bigint(20) unsigned NOT NULL COMMENT 'notice id',
+  `user_id` bigint(20) unsigned NOT NULL COMMENT '사용자 id',
+  `read_at` datetime NOT NULL COMMENT '읽은 날짜',
+  `never_show` tinyint(1) unsigned NOT NULL COMMENT '다시 보지 않기'
+) ENGINE=InnoDB DEFAULT CHARSET=armscii8 COLLATE=armscii8_bin COMMENT='notice 뷰 기록';
+
 CREATE TABLE IF NOT EXISTS `temp_users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '고유 id',
   `email` varchar(320) COLLATE utf8mb4_bin NOT NULL COMMENT '이메일 최대값',
@@ -16,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `temp_users` (
   `verify_code` varchar(32) COLLATE utf8mb4_bin NOT NULL COMMENT '인증코드',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=83 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '고유id',
@@ -26,7 +43,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `create_at` datetime NOT NULL DEFAULT current_timestamp() COMMENT '생성 날짜',
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE KEY `name` (`name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='인증된 사용자\r\n';
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='인증된 사용자\r\n';
 
 CREATE TABLE IF NOT EXISTS 'notices' (
   'notice_id' bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'notice id',
